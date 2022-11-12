682fa21e045dec772fed06f4b87cafa3a5305412
OchenSeriousNikname <ehot.tgre@gmail.com>
2022-11-12 13:52:32 +0700

    полностью работает +  удаление

--- app/src/main/java/com/example/huetana/MainActivity.kt
+++ app/src/main/java/com/example/huetana/MainActivity.kt
@@ -17,10 +17,8 @@ import com.example.huetana.R
 import android.content.ContentValues
 import android.database.Cursor
 
-data class Todo(val id: Long, val title: String, val isDone: Boolean)
-
 class MainActivity : AppCompatActivity() {
-    private val list = mutableListOf<String>()
+    private val list = mutableListOf<Todo>()
     private lateinit var adapter: RecyclerAdapter
     val dbHelper = DBHelper(this)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,10 +26,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val editText = findViewById<EditText>(R.id.editText)
 
-        list.addAll(dbHelper.getAll().map { it.title })
+        list.addAll(dbHelper.getAll())
 
         adapter = RecyclerAdapter(list) {
-            // адаптеру передали обработчик удаления элемента
+            dbHelper.remove( list[it].id)
             list.removeAt(it)
             adapter.notifyItemRemoved(it)
         }
@@ -43,122 +41,11 @@ class MainActivity : AppCompatActivity() {
         val buttonAdd = findViewById<Button>(R.id.button)
         buttonAdd.setOnClickListener {
             val title = editText.text.toString()
-            list.add(title)
+            val id = dbHelper.add(title)
+            list.add(Todo(id, title))
             adapter.notifyItemInserted(list.lastIndex)
 
-            val id = dbHelper.add(title)
 
         }
-    }
-}
-class RecyclerAdapter(
-    private val list: List<String>,
-    // передаём коллбек нажатия на кнопку
-    private val onItemClick: (id: Int) -> Unit
-) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_item, parent, false)
-        return ViewHolder(itemView)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = list[position]
-        // обработчик нажатия кнопки
-        holder.button.setOnClickListener {
-            onItemClick(holder.adapterPosition)
-        }
-    }
-
-    override fun getItemCount(): Int {
-        return list.size
-    }
-
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textView = itemView.findViewById<TextView>(R.id.textView)
-        // находим кнопку
-        val button = itemView.findViewById<Button>(R.id.button2)
-    }
-}
-
-class DBHelper(context: Context?) :
-    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
-
-    companion object {
-        const val DATABASE_VERSION = 1
-        const val DATABASE_NAME = "tododb"
-        const val TABLE_NAME = "todos"
-        const val KEY_ID = "id"
-        const val KEY_TITLE = "title"
-        const val KEY_IS_DONE = "is_done"
-    }
-
-    override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL("""
-            CREATE TABLE $TABLE_NAME (
-                $KEY_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                $KEY_TITLE TEXT NOT NULL,
-                $KEY_IS_DONE INTEGER NOT NULL
-            )""")
-    }
-
-    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
-        onCreate(db)
-    }
-
-    fun getAll(): List<Todo> {
-        val result = mutableListOf<Todo>()
-        val database = this.writableDatabase
-        val cursor: Cursor = database.query(
-            TABLE_NAME, null, null, null,
-            null, null, null
-        )
-        if (cursor.moveToFirst()) {
-            val idIndex: Int = cursor.getColumnIndex(KEY_ID)
-            val titleIndex: Int = cursor.getColumnIndex(KEY_TITLE)
-            val isDoneIndex: Int = cursor.getColumnIndex(KEY_IS_DONE)
-            do {
-                val todo = Todo(
-                    cursor.getLong(idIndex),
-                    cursor.getString(titleIndex),
-                    cursor.getInt(isDoneIndex) == 1
-                )
-                result.add(todo)
-            } while (cursor.moveToNext())
-        }
-        cursor.close()
-        return result
-    }
-
-    fun add(title: String, isDone: Boolean = false): Long {
-        val database = this.writableDatabase
-        val contentValues = ContentValues()
-        contentValues.put(KEY_TITLE, title)
-        contentValues.put(KEY_IS_DONE, if (isDone) 1 else 0)
-        val id = database.insert(TABLE_NAME, null, contentValues)
-        close()
-        return id
-    }
-
-    fun update(id: Long, title: String, isDone: Boolean) {
-        val database = this.writableDatabase
-        val contentValues = ContentValues()
-        contentValues.put(KEY_TITLE, title)
-        contentValues.put(KEY_IS_DONE, if (isDone) 1 else 0)
-        database.update(TABLE_NAME, contentValues, "$KEY_ID = ?", arrayOf(id.toString()))
-        close()
-    }
-
-    fun remove(id: Long) {
-        val database = this.writableDatabase
-        database.delete(TABLE_NAME, "$KEY_ID = ?", arrayOf(id.toString()))
-        close()
-    }
-
-    fun removeAll() {
-        val database = this.writableDatabase
-        database.delete(TABLE_NAME, null, null)
-        close()
     }
 }
