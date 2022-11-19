771b4f44f9edbaed547c53c8cc581304ebd90466
OchenSeriousNikname <ehot.tgre@gmail.com>
2022-11-19 14:42:14 +0700

    Добавил переходы. Начал осуществлять сохранение в базу данных

--- app/src/main/java/com/example/huetana/Settings.kt
+++ app/src/main/java/com/example/huetana/Settings.kt
@@ -10,21 +10,21 @@ class Settings : AppCompatActivity() {
     private val dbHelper = DBHelper(this)
     private val list = mutableListOf<Todo>()
     private lateinit var adapter: RecyclerAdapter
-    val editText1 = findViewById<EditText>(R.id.editTextTextPersonName4)
-    val editText2 = findViewById<EditText>(R.id.editTextTextPersonName2)
-    val editText3 = findViewById<EditText>(R.id.editTextTextPersonName3)
-    val editText4 = findViewById<EditText>(R.id.editTextTextPersonName)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        val editText1 = findViewById<EditText>(R.id.name)
+        val editText2 = findViewById<EditText>(R.id.surname)
+        val editText3 = findViewById<EditText>(R.id.dr)
+        val editText4 = findViewById<EditText>(R.id.number)
 
-        val buttonESC = findViewById<Button>(R.id.button6)
+        val buttonESC = findViewById<Button>(R.id.buttonCancel)
         buttonESC.setOnClickListener {
 
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-        val buttonSave = findViewById<Button>(R.id.button)
+        val buttonSave = findViewById<Button>(R.id.buttonSave)
         buttonSave.setOnClickListener {
             val title = editText1.text.toString()
             val id = dbHelper.add(title)
